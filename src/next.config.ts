66e8adb033d3b766eac1bd4d727fe1563f9206a3
Toasty <maxlowyuwen@gmail.com>
2025-12-09 07:34:45 +0000

    repo name is "my-clothing-website"
    possible to use index.html?
    
    If your

---
+++ src/next.config.ts
@@ -0,0 +1,41 @@
+import type {NextConfig} from 'next';
+
+const repoName = process.env.REPO_NAME || 'my-clothing-website';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? `/${repoName}` : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '',
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
