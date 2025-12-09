10d9bef9dcedf172e26e405576d4e850a7a22f09
Toasty <maxlowyuwen@gmail.com>
2025-12-09 06:37:50 +0000

    The `npm install` command failed in my project. Analyze the following er

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // This is to allow cross-origin requests in development.
+    allowedDevOrigins: ["**"],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
