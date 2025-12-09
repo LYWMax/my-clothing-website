20eb7930288ddc1a8c0ea1490a4baa60548bef44
Toasty <maxlowyuwen@gmail.com>
2025-12-09 06:38:36 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
+  devServer: {
     // This is to allow cross-origin requests in development.
     allowedDevOrigins: ["**"],
   },
